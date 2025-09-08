8d4bcca42666262897bb49be32d53c010d9cdbbd
Iryna9992004 <iren171302@gmail.com>
2025-09-08 20:19:12 +0300

    fix: fix feed message width

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -3,5 +3,5 @@ import React from "react";
 export default function ProtectedLayout({ children }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="bg-black w-full h-full bg-opacity-20">{children}</div>;
+  return <div className="bg-black w-full h-full bg-opacity-20 overflow-y-auto">{children}</div>;
 }
--- frontend/src/components/entities/feed-message/FeedMessage.tsx
+++ frontend/src/components/entities/feed-message/FeedMessage.tsx
@@ -16,24 +16,24 @@ const FeedMessage: React.FC<FeedMessageProps> = ({
 
   const getColorConfig = (colorNum: string | number) => {
     const key = String(colorNum) as unknown;
-    return (
-      colors[
-        key as "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10"
-      ]
-    );
+    return colors[
+      key as "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10"
+    ];
   };
 
   return (
     <div className={`w-full flex ${isMine ? "justify-end" : "justify-start"}`}>
       <div
-        className="relative flex justify-end mb-4 px-4 min-w-[80px] max-w-[35%] cursor-pointer"
+        className="relative flex justify-end mb-4 px-4 min-w-[80px] w-[30%] max-w-[45%] cursor-pointer"
         onClick={() => setRepliesOpened(!repliesOpened)}
       >
         <div className="w-full">
           <div
             className={`${getColorConfig(colorNumber).border} text-white rounded-2xl p-[1px] relative`}
           >
-            <div className={`${getColorConfig(colorNumber).bg} text-white rounded-2xl px-4 py-3 relative`}>
+            <div
+              className={`${getColorConfig(colorNumber).bg} text-white rounded-2xl px-4 py-3 relative`}
+            >
               <p className="text-md leading-relaxed">{text}</p>
               <div className="flex items-center justify-between mt-1 space-x-2">
                 <span className="text-xs text-gray-200">{timestamp}</span>
@@ -57,9 +57,13 @@ const FeedMessage: React.FC<FeedMessageProps> = ({
         </div>
 
         {isMine ? (
-          <div className={`absolute right-[9px] -bottom-[4px] w-0 h-0 border-l-[9px] border-l-transparent border-r-[9px] border-r-transparent border-b-[12px] ${getColorConfig(colorNumber).rightCorner} transform -rotate-226`} />
+          <div
+            className={`absolute right-[9px] -bottom-[4px] w-0 h-0 border-l-[9px] border-l-transparent border-r-[9px] border-r-transparent border-b-[12px] ${getColorConfig(colorNumber).rightCorner} transform -rotate-226`}
+          />
         ) : (
-          <div className={`absolute left-[9px] -bottom-[4px] w-0 h-0 border-l-[9px] border-l-transparent border-r-[9px] border-r-transparent border-b-[12px] ${getColorConfig(colorNumber).leftCorner} transform rotate-226`} />
+          <div
+            className={`absolute left-[9px] -bottom-[4px] w-0 h-0 border-l-[9px] border-l-transparent border-r-[9px] border-r-transparent border-b-[12px] ${getColorConfig(colorNumber).leftCorner} transform rotate-226`}
+          />
         )}
       </div>
     </div>
