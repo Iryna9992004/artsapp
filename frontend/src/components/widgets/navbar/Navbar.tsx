6d7ffc99946678c7738e0dc77872680317380452
Iryna9992004 <iren171302@gmail.com>
2025-09-10 18:49:59 +0300

    fix: fix dynamic routes layout

--- frontend/src/components/widgets/navbar/Navbar.tsx
+++ frontend/src/components/widgets/navbar/Navbar.tsx
@@ -12,10 +12,12 @@ export default function Navbar() {
 
   return (
     <div className="sticky top-0 left-0 right-0 w-full h-fit bg-black px-8 py-4 border-b border-white/20 flex items-center gap-4 z-1000">
-      <ChevronLeft
-        className="text-white cursor-pointer"
-        onClick={() => router.back()}
-      />
+      {searchParams.get("message") ? (
+        <ChevronLeft
+          className="text-white cursor-pointer"
+          onClick={() => router.back()}
+        />
+      ) : null}
       <h1 className="text-white font-bold text-2xl">
         {pathnames[pathname as never] || searchParams.get("message")}
       </h1>
