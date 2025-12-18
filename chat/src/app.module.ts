7f1401a4eb879da72f9761e78ceb2e63169b26f0
Iryna1713020 <iren171302@gmail.com>
2025-12-18 23:03:24 +0200

    feat: add dockerfiles

--- chat/src/app.module.ts
+++ chat/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ChatModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
