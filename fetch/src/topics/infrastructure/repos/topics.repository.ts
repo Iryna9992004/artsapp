c8724231b7976d557198288a9d565619dc6989e5
Iryna9992004 <iren171302@gmail.com>
2025-12-23 16:46:10 +0200

    feat: add create event migration

--- fetch/src/topics/infrastructure/repos/topics.repository.ts
+++ fetch/src/topics/infrastructure/repos/topics.repository.ts
@@ -8,24 +8,26 @@ import { TopicRepository } from 'src/topics/domain/repos/topic.repository';
 export class TopicRepositoryClickhouse implements TopicRepository {
   constructor(private readonly clickhouseService: ClickhouseService) {}
 
-  async search(limit: number, offset: number, text?: string): Promise<Topic[]> {
+  async search(offset: number, limit: number, text?: string): Promise<Topic[]> {
     try {
       if (text && text.length > 0) {
         const rows = await this.clickhouseService.getConfig().query({
           query: `SELECT t.*,
-          ifNull(r.reads_count, 0) AS reads_count 
-          FROM ${config.db.name}.topics t
-          LEFT JOIN (
-            SELECT
-              topic_id,
-              count() AS reads_count
-            FROM ${config.db.name}.topic_reads
-            GROUP BY topic_id
-          ) r
-          ON r.topic_id = t.id
-          WHERE text LIKE '${text}%' 
-          ORDER BY id OFFSET ${limit} 
-          ROW FETCH FIRST ${offset} ROWS ONLY`,
+      u.full_name AS author_name,
+      ifNull(r.reads_count, 0) AS reads_count 
+      FROM ${config.db.name}.topics t
+      LEFT JOIN ${config.db.name}.users u ON u.id = t.user_id
+      LEFT JOIN (
+        SELECT
+          topic_id,
+          count() AS reads_count
+        FROM ${config.db.name}.topic_reads
+        GROUP BY topic_id
+      ) r ON r.topic_id = t.id
+      WHERE t.text LIKE '${text}%' 
+      ORDER BY t.id 
+      LIMIT ${limit} 
+      OFFSET ${offset}`,
           format: 'JSONEachRow',
         });
 
@@ -33,7 +35,21 @@ export class TopicRepositoryClickhouse implements TopicRepository {
         return topics;
       }
       const rows = await this.clickhouseService.getConfig().query({
-        query: `SELECT * FROM ${config.db.name}.topics ORDER BY id OFFSET ${limit} ROW FETCH FIRST ${offset} ROWS ONLY`,
+        query: `SELECT t.*,
+        u.full_name AS author_name,
+        ifNull(r.reads_count, 0) AS reads_count
+        FROM ${config.db.name}.topics t
+        LEFT JOIN ${config.db.name}.users u ON u.id = t.user_id
+        LEFT JOIN (
+          SELECT 
+            topic_id,
+            count() AS reads_count
+          FROM ${config.db.name}.topic_reads
+          GROUP BY topic_id
+        ) r ON r.topic_id = t.id
+        ORDER BY t.id 
+        LIMIT ${limit} 
+        OFFSET ${offset}`,
         format: 'JSONEachRow',
       });
 
